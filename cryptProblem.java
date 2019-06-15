f525333e2ce0d037346d98af9393f705c4d2da8a
Ashley <28584443+tank-wsp@users.noreply.github.com>
2019-06-15 14:31:35 -0400

    Updated code

--- cryptProblem.java
+++ cryptProblem.java
@@ -2,7 +2,7 @@
  * Ashley Clark 
  * A program that takes the user's string input that is divisible by 3
  * then calculates how many changes the string needs for it to read PER until the end of the users input
- * So if a user enters SECRET how the output would be 4 because it took 4 changes to get "SECRET" to read "PERPER"
+ * So if a user enters SECRET the output would be 4 because it took 4 changes to get "SECRET" to read "PERPER"
  */
 
 
@@ -16,14 +16,32 @@ public class cryptProblem {
 		System.out.print("Please enter a string that is divisable by 3:");
 		String userCreatedStr = scanner.nextLine();//Takes the user's input and stores it in the userCreatedstr var
 		String name="PER";// String to compare the user's input with
+		
+		int userStrLength= userCreatedStr.length();//keeps track of the length of the user's string
+		int nameLength= name.length();//keeps track of "PER" length
 		int count=0;// variable to keep track of the number of changes needed in the user's string
+		String replaceString= userCreatedStr.replace(userCreatedStr, name);
 		
 		if (userCreatedStr.equals(name)) {// check to see if the user's input matches exactly without change
 			System.out.print("0");// Prints zero because no change is needed.
 		}
+		else
+			System.out.print(replaceString);
 			
+			//for(int i= 0; i<=userCreatedStr.length(); i++) {
+	//	String replaceString= userCreatedStr.replace(userCreatedStr, "PER");
+			//}
+			
+			//if (userCreatedStr.length()==name.length()) {
+				
+				
+			}
 		}
+	
+		
+			
+		
 
-	}
+	
 
 
